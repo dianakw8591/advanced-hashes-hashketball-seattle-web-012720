e23c3b6b73e589c1e1132b90a14a92139a2ed935
Diana Wendt <dianakw@gmail.com>
2020-01-27 05:51:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,7 +172,8 @@ def most_points_scored
 end
 
 def point_total(side)
-  game_hash.reduce(nil) do |total, 
+  game_hash.each do {|(side, info_hash)|
+    info_hash[:players]reduce(nil) do |total, 
 end
 
 def winning_team
